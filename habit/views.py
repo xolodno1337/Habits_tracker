783942b7f8830a049d492d1655cdcc3f57ebe08e
Сергей Бутаков <xolodnotchk@mail.ru>
2024-09-12 15:44:30 +0300

    Добавление pagination_class для class HabitViewSet

--- habit/views.py
+++ habit/views.py
@@ -1,6 +1,7 @@
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 from habit.models import Habit
+from habit.paginations import CustomPagination
 from habit.serializers import HabitSerializer
 from users.permissions import IsOwner
 
@@ -8,6 +9,7 @@ from users.permissions import IsOwner
 class HabitViewSet(ModelViewSet):
     queryset = Habit.objects.all()
     serializer_class = HabitSerializer
+    pagination_class = CustomPagination
 
     def perform_create(self, serializer):
         habit = serializer.save()
