973af7d2ea15f737e69bfec9e646e9b10732491c
Сергей Бутаков <xolodnotchk@mail.ru>
2024-09-12 15:20:21 +0300

    Добавление permission_classes для class UserCreateAPIView

--- users/views.py
+++ users/views.py
@@ -1,4 +1,6 @@
 from rest_framework.generics import CreateAPIView
+from rest_framework.permissions import AllowAny
+
 from users.models import User
 from users.serializers import UserSerializer
 
@@ -6,6 +8,7 @@ from users.serializers import UserSerializer
 class UserCreateAPIView(CreateAPIView):
     serializer_class = UserSerializer
     queryset = User.objects.all()
+    permission_classes = (AllowAny,)
 
     def perform_create(self, serializer):
         user = serializer.save(is_active=True)
