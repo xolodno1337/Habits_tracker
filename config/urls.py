f4de27ae19deac32b7634d1556fd90de08377861
Сергей Бутаков <xolodnotchk@mail.ru>
2024-09-12 14:16:50 +0300

    Добавление path('users/')

--- config/urls.py
+++ config/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('habit/', include('habit.urls', namespace='habit'))
+    path('habit/', include('habit.urls', namespace='habit')),
+    path('users/', include('users.urls', namespace='users'))
 ]
