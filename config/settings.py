f5ad9db2b2ee30d764b5caffc29fd5a0bc8efe80
Сергей Бутаков <xolodnotchk@mail.ru>
2024-09-12 12:11:21 +0300

    добавление приложений 'users', 'habit' и AUTH_USER_MODEL

--- config/settings.py
+++ config/settings.py
@@ -21,6 +21,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'users',
+    'habit',
 ]
 
 MIDDLEWARE = [
@@ -93,3 +95,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+AUTH_USER_MODEL = 'users.User'
