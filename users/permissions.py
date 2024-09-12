961c1b8d54e64cd89a5890732337e60a2ad686b7
Сергей Бутаков <xolodnotchk@mail.ru>
2024-09-12 15:21:09 +0300

    Добавление class IsOwner

---
+++ users/permissions.py
@@ -0,0 +1,9 @@
+from rest_framework import permissions
+
+
+class IsOwner(permissions.BasePermission):
+    """ Проверяет, является ли пользователь владельцем. """
+    def has_object_permission(self, request, view, obj):
+        if obj.user == request.user:
+            return True
+        return False
