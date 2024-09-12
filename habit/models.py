37c07b65f52cd6ac4f13c9135ddb5a670f36eae4
Сергей Бутаков <xolodnotchk@mail.ru>
2024-09-12 13:20:48 +0300

    Изменение типов полей, добавление def __str__

--- habit/models.py
+++ habit/models.py
@@ -1,3 +1,5 @@
+from datetime import timedelta
+
 from django.db import models
 from users.models import User
 
@@ -7,8 +9,7 @@ NULLABLE = {'blank': True, 'null': True}
 class Habit(models.Model):  # Привычка
     user = models.ForeignKey(User, verbose_name='Пользователь', on_delete=models.CASCADE, **NULLABLE)
     place = models.TextField(verbose_name='Место', help_text='Укажите место выполнения привычки')
-    time = models.DateTimeField(auto_now=False, auto_now_add=False, verbose_name='Время',
-                                help_text='Укажите время выполнения привычки', **NULLABLE)
+    time = models.TimeField(verbose_name='Время', help_text='Укажите время выполнения привычки', **NULLABLE)
     action = models.CharField(max_length=50, verbose_name='Действие',
                               help_text='Укажите действие, которое представляет собой привычка')
     pleasant_habit = models.BooleanField(default=True, verbose_name='Признак приятной привычки',
@@ -19,10 +20,14 @@ class Habit(models.Model):  # Привычка
                                       help_text='Укажите периодичность выполнения привычки(по умолчанию ежедневная)')
     reward = models.CharField(max_length=150, verbose_name='Вознаграждение',
                               help_text='Укажите, чем хотите себя вознаградить после выполнения', **NULLABLE)
-    time_to_complete = models.TimeField(auto_now=False, auto_now_add=False, verbose_name='Время на выполнение',
-                                        help_text='Время, которое предположительно потратите', **NULLABLE)
+    time_to_complete = models.DurationField(verbose_name='Время на выполнение',
+                                            help_text='Время, которое предположительно потратите',
+                                            default=timedelta(minutes=2), **NULLABLE)
     is_published = models.BooleanField(default=True, verbose_name='Признак публичности',
                                        help_text='Можно опубликовать в общий доступ?', **NULLABLE)
+
+    def __str__(self):
+        return f'Я буду {self.action} в {self.time} в {self.place}'
 
     class Meta:
         verbose_name = 'Привычка'
